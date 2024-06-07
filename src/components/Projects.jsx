b924cb6ef93af34b6c05ced339e037afe9ba0e9b
Ajith kannan <84245047+ajithyaasaf@users.noreply.github.com>
2024-06-07 20:51:40 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,8 +17,7 @@ const Projects = () => {
          },
          {
             image:projectImage3,
-            description:'This is a Admin DashBoard for DentalCare Hospital build using JavaScript,ReactJs,BootStrap,ExpressJs,
-               Firebase.',
+            description:'This is a Admin DashBoard for DentalCare Hospital build using JavaScript,ReactJs,BootStrap,ExpressJs,Firebase.',
             link:'https://hospital-management-23dbf.web.app/'
          }
       ]
